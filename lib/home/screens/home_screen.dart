e3bf2604cfac9c76071db469cdef9ef72ab2c606
MoBebba123 <mohamedbebba1@gmail.com>
2022-12-24 13:55:19 +0100

    home screen

--- lib/home/screens/home_screen.dart
+++ lib/home/screens/home_screen.dart
@@ -1,8 +1,7 @@
-import 'package:ecommerce/providers/user_provider.dart';
+import 'package:ecommerce/constants/global_variables.dart';
+import 'package:ecommerce/home/widgets/address_box.dart';
+import 'package:ecommerce/home/widgets/top_categories.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:provider/provider.dart';
 
 class HomeScreen extends StatefulWidget {
   static const String routeName = "/home-screen";
@@ -15,10 +14,89 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    final user = Provider.of<UserProvider>(context).user;
     return Scaffold(
-      body: Center(
-        child: Text(user.toJson()),
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(70),
+        child: AppBar(
+          flexibleSpace: Container(
+            decoration: const BoxDecoration(
+              gradient: GlobalVariables.appBarGradient,
+            ),
+          ),
+          title: Padding(
+            padding: const EdgeInsets.only(top: 12.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: Container(
+                    height: 42,
+                    margin: const EdgeInsets.only(left: 15),
+                    child: Material(
+                      borderRadius: BorderRadius.circular(7),
+                      elevation: 1,
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          prefixIcon: InkWell(
+                            onTap: () {},
+                            child: const Padding(
+                              padding: EdgeInsets.only(
+                                left: 6,
+                              ),
+                              child: Icon(
+                                Icons.search,
+                                color: Colors.black,
+                                size: 23,
+                              ),
+                            ),
+                          ),
+                          filled: true,
+                          fillColor: Colors.white,
+                          contentPadding: const EdgeInsets.only(top: 10),
+                          border: const OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(7),
+                            ),
+                            borderSide: BorderSide.none,
+                          ),
+                          enabledBorder: const OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(7),
+                            ),
+                            borderSide: BorderSide(
+                              color: Colors.black38,
+                              width: 1,
+                            ),
+                          ),
+                          hintText: 'Search for products',
+                          hintStyle: const TextStyle(
+                            fontWeight: FontWeight.w500,
+                            fontSize: 17,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  color: Colors.transparent,
+                  height: 42,
+                  margin: const EdgeInsets.symmetric(horizontal: 10),
+                  child: const Icon(Icons.mic, color: Colors.black, size: 25),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+      body: Column(
+        children: const [
+          AddressBox(),
+          SizedBox(
+            height: 10,
+          ),
+          TopCategories(),
+        ],
       ),
     );
   }
---
+++ lib/home/widgets/deal_of_day.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DealOfDay extends StatefulWidget {
+  const DealOfDay({Key? key}) : super(key: key);
+
+  @override
+  _DealOfDayState createState() => _DealOfDayState();
+}
+
+class _DealOfDayState extends State<DealOfDay> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
